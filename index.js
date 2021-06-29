c728fab5667909a4192dce1c94a233337728cea1
accrossmindanduniverse <fajarmccartney@gmail.com>
2021-06-29 17:47:02 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 8080
+// const env = process.env
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const connection = require('./src/helpers/db')
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`App runningg on port ${port}`)
+})
+
+connection.connect(function (err) {
+  if (err) {
+    throw err
+  } else {
+    console.log('Database has connected')
+  }
+})
