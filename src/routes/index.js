335c7b746941ef8ba6f87f73cce3fd23f63ae30c
accrossmindanduniverse <fajarmccartney@gmail.com>
2021-06-30 09:11:25 +0700

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const authRoute = require('./auth')
 
 // prefix('/')
+
+router.use('/auth', authRoute)
+
 module.exports = router
