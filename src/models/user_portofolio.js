308666aa4d6174196fd46ec329ecc2081527bb35
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-06-30 11:34:04 +0700

    GET portofolio by id user (Zidan)

---
+++ src/models/user_portofolio.js
@@ -0,0 +1,12 @@
+const { promisify } = require('util')
+const db = require('../helpers/db')
+
+const execPromise = promisify(db.query).bind(db)
+
+module.exports = {
+
+  getPortofoliosByIdUser: (id) => {
+    return execPromise('SELECT user_portofolio.id, user_portofolio.id_user, user_portofolio.app_name, user_portofolio.link_repo, user_portofolio.type, user_portofolio.picture, user_portofolio.created_at, user_portofolio.updated_at FROM user_portofolio INNER JOIN user ON user.id = user_portofolio.id_user WHERE user.id = ?', [id])
+  }
+
+}
