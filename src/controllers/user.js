07c966e25857de898a2f8b87b70602950fb22808
hafidz1605 <hafidzhidayat1605@gmail.com>
2021-07-02 15:07:26 +0700

    add string to array

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,6 +9,11 @@ exports.getDetailTalent = (req, res) => {
   userModel.getTalentById(id, (err, results) => {
     if (!err && results.length === 1) {
       const talent = results[0]
+
+      if (talent.picture !== null) {
+        talent.picture = `${process.env.APP_URL}/${talent.picture}`
+      }
+
       response(res, true, talent, 200)
     } else {
       response(res, false, 'talent not forund', 404)
@@ -129,7 +134,7 @@ exports.updateUserRecruiterImage = (req, res) => {
             }
           })
         } else {
-          fs.unlinkSync(path + '/' + results[0].picture)
+          // pake fs
           userModel.updateUserRecruiterImage(updateData, (err, results) => {
             if (!err) {
               return response(res, true, results, 200)
