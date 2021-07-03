d9a814728ea837474be88934f198d3c26c0f8885
hafidz1605 <hafidzhidayat1605@gmail.com>
2021-07-03 12:28:37 +0700

    add detail

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ const execPromise = promisify(db.query).bind(db)
 
 exports.getTalentById = (id, cb) => {
   db.query(`
-  SELECT picture,full_name,job_desk,address,job_type,description
+  SELECT picture,full_name,job_desk,address,job_type,description,email,phone_number
   FROM user_talent 
   LEFT JOIN user ON user_talent.id_user = user.id
   WHERE id_user = ?
